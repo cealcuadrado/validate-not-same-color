a76615ec9ec214de3e81a90e06aa9d2bef7e8d1f
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-05 11:14:26 -0300

    Definición de formulario responsivo funcional

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,7 @@
 import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ColorEvent } from 'ngx-color';
 
 @Component({
   selector: 'app-layout',
@@ -8,11 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  colorForm: FormGroup;
+
   constructor(
+    private fb: FormBuilder,
     private toastr: ToastrService
   ) { }
 
   ngOnInit() {
-    this.toastr.success("Hola, mundo!");
+    this.colorForm = this.fb.group({
+      colorBackground: this.fb.control('', Validators.required),
+      colorFont: this.fb.control('', Validators.required)
+    });
+  }
+
+  setColorBackground(event: ColorEvent): void {
+    this.colorForm.controls['colorBackground'].setValue(event.color.hex);
+  }
+
+  setColorFont(event: ColorEvent): void {
+    this.colorForm.controls['colorFont'].setValue(event.color.hex);
+  }
+
+  onSubmit(): void {
+    let value = this.colorForm.value;
+    this.toastr.success(`Color de fondo: ${value.colorBackground}, Color de fuente: ${value.colorFont}`)
+  }
+
+  resetForm(): void {
+    this.colorForm.reset();
   }
 }
