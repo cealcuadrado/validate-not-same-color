d76d7e03282c1136f8097e1e61f80d2b0c202010
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-05 10:57:13 -0300

    Instalados paquetes principales, definido layout principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 
+import { ColorCircleModule } from 'ngx-color/circle';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ColorCircleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,3 +1,4 @@
+import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.toastr.success("Hola, mundo!");
   }
-
 }
